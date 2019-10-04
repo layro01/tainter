2f0e154ec9f83aa4470ee2e4d5ad3716d8591626
Rob Layzell <robert.layzell@ca.com>
2019-10-04 14:39:11 -0700

    Updated to show new use case.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const IAST = require('./iast-sdk');
 // Mock an HTTP request object.
 const requestMock = new Object();
 requestMock.query = new Object();
-requestMock.query.param1 = IAST.taintObject('1'); // This would be tainted in the real application
+requestMock.query.param1 = '1'; // This would be tainted in the real application
 
-// const taintedRequestMock = IAST.taintObject(request);
+const taintedRequestMock = IAST.taintObject(requestMock);
 
-const value1 = eval(requestMock.query.param1);
+const value1 = eval(taintedRequestMock.query.param1);
 
 console.log(value1);
